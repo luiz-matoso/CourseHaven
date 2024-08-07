606875cb4b5b131b6f5f9ba0fbe486f661f57903
luizmatoso <luizhmatoso1340@gmail.com>
2024-08-07 14:25:38 -0300

    Project created

---
+++ src/main/java/com/luizmatoso/CourseHaven/CourseHavenApplication.java
@@ -0,0 +1,13 @@
+package com.luizmatoso.CourseHaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseHavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizmatoso/CourseHaven/CourseHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmatoso.CourseHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
